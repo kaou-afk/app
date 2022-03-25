3d20dd871e2f9568d23a29b44e2464dad7771b60
kaourantin-elain <kaourantin.elain@stfelixlasalle.fr>
2022-03-25 11:57:09 +0100

    hjh
    ghh

--- src/app/automatic/automatic.page.ts
+++ src/app/automatic/automatic.page.ts
@@ -29,11 +29,40 @@ export class AutomaticPage implements OnInit {
     if (this.start==0){
       document.getElementById('start').style.setProperty('background' , '#1DAEFF');
       this.start=1;
+      
     }
     else{
       document.getElementById('start').style.setProperty('background' , '#616161');
       this.start=0;
     }
 
+  }
+
+  automatic(){
+    
+    while (this.start == 1) {
+      var nb = this.getRandomInt(4);
+      switch (nb) {
+        case 1:
+          console.log('1');
+          break;
+
+        case 2:
+          console.log('2');
+          break;  
+            
+        case 3:
+          console.log('3');
+          break;   
+
+        case 4:
+          console.log('4');
+          break;       
+      }
+    }
+  }
+
+  getRandomInt(max) {
+    return Math.floor(Math.random() * (max - 1 + 1) + 1);;
   }
 }
