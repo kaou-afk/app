152bd64b2d2f91742835e0e4c6c53a5d3c8ff096
kaourantin-elain <kaourantin.elain@stfelixlasalle.fr>
2022-03-31 21:42:00 +0200

    dd
    dd

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -16,19 +16,19 @@ export class HomePage {
   }
 
   public scoreList = [];
-  public List = [2,9,6,45,6];
 
   API(){
-    this.http.get('http://localhost/api/resultats/rechercherScore.php?idUtilisateur=8') .subscribe((data) => { 
+    this.http.get('http://localhost/api/resultats/rechercherScore.php?idUtilisateur=1') .subscribe((data) => { 
       this.scoreList = Object.values(data['Resultats']);
       console.log(this.scoreList);
+      this.barChartData = [
+        {data: this.scoreList,borderColor: '#1DAEFF', pointBackgroundColor: 'rgba(148,159,177,1)',
+        pointBorderColor: '#fff',}
+      ];
     });
   }
 
-  public barChartData = [
-    {data: this.List,borderColor: '#1DAEFF', pointBackgroundColor: 'rgba(148,159,177,1)',
-    pointBorderColor: '#fff',}
-  ];
+  public barChartData :Array<any> = [];
   public barChartLabels = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
   public barChartType = 'line';
   public barChartLegend = false;
